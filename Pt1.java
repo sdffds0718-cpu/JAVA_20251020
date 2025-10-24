25b36fea1e2b536c676b219e6d4d890aca63be44
sdffds0718-cpu <sdffds0718@gmail.com>
2025-10-24 18:58:31 +0900

    for 구문과 while구문을 복습하기위해 연습함

---
+++ Pt1.java
@@ -0,0 +1,107 @@
+package ex01;
+
+public class Pt1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1부터 10까지의 숫자를 for문으로
+		 */
+	/*	int sum = 0;
+
+	for (int i = 1; i<=10; i++) {
+		sum += i;
+	}
+System.out.println("1부터 10까지의 합은" + sum + "입니다.");
+	}*/
+		/*
+		 * 1부터 100까지의 합을 구하고 출력하세요
+		 * 1부터 100까지의 합은 5050입니다.
+		 */
+
+/*		int sum = 0;
+		for (int i =1; i<=100; i++) {
+			sum += i;
+			
+		}
+		System.out.println("1부터 100까지의 합은" + sum + "입니다");
+		
+}
+*/
+		/*
+		 * 1부터 5까지의 숫자를 while문으로 출력하세요
+		 */
+	/*	int i = 1;
+		int sum = 0;
+		while (i<=5) {
+			System.out.println(i);
+			sum+=i;
+			i++;
+		
+		}
+		System.out.println("1부터 5까지의 숫자는" + sum + "입니다");
+}*/
+	/*
+	 * 합계가 200을 초과했습니다. 현재 값: 210
+	 */
+	/*	int i = 1;
+		int sum = 0;
+		while (sum<=200) {
+			sum += i;
+			i++;
+			
+		}
+		
+		System.out.println("합계가 200을 초과했습니다. 현재 값:" +sum );
+	*/
+		/*
+		 * 1부터 20까지의 숫자 중 짝수만 출력하세요 for문.
+		 
+		int sum = 0;
+	for (int i = 2; i <= 20; i++) {
+	sum = i;
+		if (sum % 2 == 0)
+		System.out.println(sum);
+	
+	}
+	*/
+		/*
+		 * 1부터 50까지의 숫자중 홀수의 합을 구하고 출력하세요
+		 */
+	/*int sum = 0;
+	for (int i = 1; i<=50; i++) {
+		sum+= i;
+		if (sum % 2 == 1) {
+		}
+	}
+	System.out.println("1부터 50까지의 숫자중 홀수의 합은" +sum + "입니다");
+	
+	}*/
+		/*
+		 * whil문을 사용해 1부터 10까지의 숫자를 한 줄에 공백으로 구분해서
+		 * 출력하세요
+		 */
+	
+	/*int i = 1;
+	int sum = 0;
+	
+	while (i<10) {
+		sum = i;
+		i++;
+		System.out.print(i+"  ");
+	}
+	
+	}*/
+		/*
+		 * while문으로 숫자를 1부터 계속 더하다가, 합계가 100을 넘는 순간
+		 * 멈추고 그 합계를 출력하세요
+		 */
+		int i = 1;
+		int sum = 0;
+		while (i <= 100) {
+			sum += i;
+			i++;
+			
+		}
+		System.out.println("1부터 100까지의 합은" + sum + "입니다.");
+	}
+	}
